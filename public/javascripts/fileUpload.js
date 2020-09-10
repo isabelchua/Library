7be9f15ed6a2006583fa5af9b2877d4f28349edd
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-09-10 13:14:51 -0700

    added image upload feature

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -2,6 +2,12 @@ FilePond.registerPlugin(
 	FilePondPluginImagePreview,
 	FilePondPluginImageResize,
 	FilePondPluginFileEncode
-)
-FilePond.parse(document.body)
+);
 
+FilePond.setOptions({
+	stylePanelAspectRatio: 150 / 100,
+	imageResizeTargetWidth: 100,
+	imageResizeTargetHeight: 150
+});
+
+FilePond.parse(document.body);
